8e646d1eb3e68c8ec369863fa8f6500d540da8b7
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2024-08-24 11:31:51 -0700

    upped the version

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ import os
 project = 'Mercury'
 copyright = '2024, Ahsan Ahmed'
 author = 'Ahsan Ahmed'
-release = '0.81'
+release = '0.82'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
